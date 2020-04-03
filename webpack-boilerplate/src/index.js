47d6196dc40ccb2cc269036faf5f030a06c5263e
encrypticus <encrypticus@gmail.com>
2020-04-03 13:34:58 +0600

    Добавил новые компоненты
    Добавил блок page-header, компонент hamburger.

--- webpack-boilerplate/src/index.js
+++ webpack-boilerplate/src/index.js
@@ -1,3 +1,5 @@
 import './sass/common.scss';
 import '../../themes/alchemy/layout/components/socials/socials';
 import '../../themes/alchemy/layout/components/site-logo/site-logo';
+import '../../themes/alchemy/layout/components/hamburger/hamburger';
+import '../../themes/alchemy/layout/blocks/page-header/page-header';
