4c96cdb3577af2d62315aa86b1e7df4fe8472635
encrypticus <encrypticus@gmail.com>
2020-04-10 12:05:00 +0600

    component site-logo: рефакторинг.

--- themes/alchemy/layout/components/site-logo/site-logo.js
+++ themes/alchemy/layout/components/site-logo/site-logo.js
@@ -2,6 +2,6 @@ import './site-logo.scss';
 
 const injector = require('svg-injector/svg-injector');
 
-const logoIcon = document.querySelector('.site-logo__icon');
+const logoIcon = document.querySelectorAll('.site-logo__icon');
 
 injector(logoIcon);
