dc2de2a200067f80aa9c668e1c81d0f1196ce0e9
encrypticus <encrypticus@gmail.com>
2020-04-10 12:04:21 +0600

    Добавил блок page-footer.

--- webpack-boilerplate/src/index.js
+++ webpack-boilerplate/src/index.js
@@ -11,3 +11,4 @@ import '#components/heading/heading';
 import '#components/post/post';
 import '#blocks/post-list/post-list';
 import '#components/button-more/button-more';
+import '#blocks/page-footer/page-footer';
