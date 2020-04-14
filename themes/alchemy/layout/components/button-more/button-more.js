82087be8ca5e6957371fa7577eb55913b68bd9e6
encrypticus <encrypticus@gmail.com>
2020-04-14 20:01:14 +0600

    component button-more: рефакторинг.

--- themes/alchemy/layout/components/button-more/button-more.js
+++ themes/alchemy/layout/components/button-more/button-more.js
@@ -1,1 +1,10 @@
 import './button-more.scss';
+
+const btnBack = document.querySelector('.button-more_type_back');
+
+if (btnBack !== null) {
+  btnBack.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.history.back();
+  });
+}
